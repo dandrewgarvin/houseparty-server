be4c52d39941fb81d53dbe7e875a49400a5534b4
Andrew Garvin <me@andrewgarv.in>
2020-04-12 21:54:37 -0600

    connect to database

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const massive = require('massive');
+const massive = require('massive');
 
 const { ApolloServer } = require('apollo-server-express');
 const schema = require('./graphql/schema.js');
@@ -17,47 +17,41 @@ function init() {
     app.use(cors());
     app.use(bodyParser.json());
 
-    // massive({
-    //   host: process.env.DB_HOST,
-    //   user: process.env.DB_USERNAME,
-    //   password: process.env.DB_PASSWORD,
-    //   port: process.env.DB_PORT,
-    //   database: process.env.DB_DATABASE,
-    //   ssl: true,
-    // }).then(db => {
-    //   console.log('✅ Database Initialized');
-    //   app.set('db', db);
+    massive(process.env.DATABASE_URI).then(db => {
+      console.log('✅ Database Initialized');
 
-    // =================== DEFINE GRAPHQL ROUTES ==================== //
-    const server = new ApolloServer({
-      schema: schema.default,
-      context: () => {
-        return {
-          // db: db,
-        };
-      },
-    });
+      app.set('db', db);
 
-    server.applyMiddleware({ app, path: '/graphql' });
-
-    // ===================== DEFINE REST ROUTES ===================== //
-    app.get('/health', (req, res) => {
-      res.status(200).send('Working');
-    });
-
-    // routes(app);
-
-    // ======================== START SERVER ======================== //
-    if (!module.parent) {
-      const PORT = process.env.PORT || 4000;
-      app.listen(PORT, () => {
-        console.log(`🚀 Listening on port ${PORT}`);
+      // =================== DEFINE GRAPHQL ROUTES ==================== //
+      const server = new ApolloServer({
+        schema: schema.default,
+        context: () => {
+          return {
+            db: db,
+          };
+        },
       });
-    }
 
-    resolve(app);
+      server.applyMiddleware({ app, path: '/graphql' });
+
+      // ===================== DEFINE REST ROUTES ===================== //
+      app.get('/health', (req, res) => {
+        res.status(200).send('Working');
+      });
+
+      // routes(app);
+
+      // ======================== START SERVER ======================== //
+      if (!module.parent) {
+        const PORT = process.env.PORT || 4000;
+        app.listen(PORT, () => {
+          console.log(`🚀 Listening on port ${PORT}`);
+        });
+      }
+
+      resolve(app);
+    });
   });
-  // });
 }
 
 if (!module.parent) {
